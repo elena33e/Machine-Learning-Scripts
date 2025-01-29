d5da496230fa13a68235be8f6c57980dbe9cc863
elena33e <108981608+elena33e@users.noreply.github.com>
2025-01-29 15:37:20 +0200

    Update Proiect - Clusterizare DBSCAN.py
    modify path to csv

--- Proiect - Clusterizare DBSCAN.py
+++ Proiect - Clusterizare DBSCAN.py
@@ -10,7 +10,7 @@ from sklearn.metrics import davies_bouldin_score
 
 
 # Load dataset
-data = pd.read_csv("C:/Users/elena/OneDrive/Desktop/Cursuri master/ML/mental_health/Mental_health_Preprocessed_MICE.csv", low_memory=False)
+data = pd.read_csv("https://github.com/elena33e/Machine-Learning-Scripts/blob/main/Mental_health_Preprocessed_MICE.csv", low_memory=False)
 
 # Verifică structura datelor
 print(data.head())  # Primele 5 rânduri
