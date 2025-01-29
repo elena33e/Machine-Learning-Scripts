aaa24c93efb7eee0e69f7eae54b090dd8d945059
elena33e <108981608+elena33e@users.noreply.github.com>
2025-01-29 15:34:49 +0200

    Update Proiect - Clasificare KNN.py
    modify path to csv file

--- Proiect - Clasificare KNN.py
+++ Proiect - Clasificare KNN.py
@@ -18,7 +18,7 @@ from sklearn.decomposition import PCA
 from mlxtend.plotting import plot_decision_regions
 
 # Încarcă dataset-ul cu low_memory=False
-data = pd.read_csv("C:/Users/elena/OneDrive/Desktop/Cursuri master/ML/mental_health/Mental_health_Preprocessed_MICE.csv", low_memory=False)
+data = pd.read_csv("https://github.com/elena33e/Machine-Learning-Scripts/blob/main/Mental_health_Preprocessed_MICE.csv", low_memory=False)
 
 # Verifică structura datelor
 print(data.head())  # Primele 5 rânduri
